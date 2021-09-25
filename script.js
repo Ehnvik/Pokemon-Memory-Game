26d6b8db09ccb70e55cd450177d8db8eb6299c72
Ehnvik <gustav.ehnvik@gmail.com>
2021-09-25 15:55:04 +0000

    added functions for card match and shuffle function

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
-const cards = document.querySelectorAll('.memory-card')
+const cards = document.querySelectorAll('.memory-card');
+
+let flippedCard = false;
+let lockBoardGame = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip')
+  if (lockBoardGame) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  checkForSameCard();
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+function checkForSameCard() {
+  let sameCard = firstCard.dataset.pokemon === secondCard.dataset.pokemon;
+  sameCard ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoardGame();
+}
+
+function unflipCards() {
+  lockBoardGame = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoardGame();
+  }, 1500);
+}
+
+function resetBoardGame() {
+  [flippedCard, lockBoardGame] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
