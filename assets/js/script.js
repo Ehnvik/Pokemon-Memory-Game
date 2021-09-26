6f0b16181cfac786f3976e16c3525ed75edb4d66
Ehnvik <gustav.ehnvik@gmail.com>
2021-09-26 18:22:01 +0000

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
+//Store the memory-card content in the variable cards.
 const cards = document.querySelectorAll('.memory-card');
 
 let flippedCard = false;
 let lockBoardGame = false;
 let firstCard, secondCard;
-
+//Make first and second card flip.
 function flipCard() {
   if (lockBoardGame) return;
   if (this === firstCard) return;
@@ -19,20 +20,20 @@ function flipCard() {
   secondCard = this;
   checkForSameCard();
 }
-
+//Check if cards match.
 function checkForSameCard() {
   let sameCard = firstCard.dataset.pokemon === secondCard.dataset.pokemon;
   sameCard ? disableCards() : unflipCards();
 }
 
-
+//Clicked cards can't be clicked again.
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 
   resetBoardGame();
 }
-
+//Clicked cards flip back after som time.
 function unflipCards() {
   lockBoardGame = true;
 
@@ -48,7 +49,7 @@ function resetBoardGame() {
   [flippedCard, lockBoardGame] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
-
+//Make the cards appear in random positions on the board.
 (function shuffle() {
   cards.forEach(card => {
     let randomPosition = Math.floor(Math.random() * 12);
